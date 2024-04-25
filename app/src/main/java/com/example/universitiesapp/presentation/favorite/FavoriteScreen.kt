bfda6832ce1e8c15033c37871ba66f2b6d540eb6
Arif Demiryas <73065590+arif-cloud@users.noreply.github.com>
2024-04-25 15:36:24 +0300

    added snackBar

--- app/src/main/java/com/example/universitiesapp/presentation/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/example/universitiesapp/presentation/favorite/FavoriteScreen.kt
@@ -14,11 +14,14 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.toMutableStateList
@@ -39,7 +42,8 @@ fun FavoriteScreen(
     viewModel: FavoriteViewModel = hiltViewModel()
 ) {
     val state by viewModel.favoriteState.collectAsState()
-    val phoneCallPermissionResultLauncher = permissionResultLauncher()
+    val snackBarHostState = remember { SnackbarHostState() }
+    val phoneCallPermissionResultLauncher = permissionResultLauncher(snackBarHostState)
     val expandedUniversityIndexes = rememberSaveable(
         saver = listSaver(
             save = { stateList -> stateList.toList() },
@@ -67,6 +71,9 @@ fun FavoriteScreen(
                     )
                 }
             )
+        },
+        snackbarHost = {
+            SnackbarHost(hostState = snackBarHostState)
         },
         content = {paddingValues ->
             Box(
--- app/src/main/java/com/example/universitiesapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/universitiesapp/presentation/home/HomeScreen.kt
@@ -17,9 +17,12 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.toMutableStateList
@@ -50,7 +53,8 @@ fun HomeScreen(
     navController: NavController,
     viewModel: HomeViewModel = hiltViewModel()
 ) {
-    val phoneCallPermissionResultLauncher = permissionResultLauncher()
+    val snackBarHostState = remember { SnackbarHostState() }
+    val phoneCallPermissionResultLauncher = permissionResultLauncher(snackBarHostState)
     val expandedProvinceIndexes = rememberSaveable(
         saver = listSaver(
             save = { stateList -> stateList.toList() },
@@ -90,6 +94,9 @@ fun HomeScreen(
                     )
                 }
             )
+        },
+        snackbarHost = {
+            SnackbarHost(hostState = snackBarHostState)
         },
         content = {paddingValues ->
 
