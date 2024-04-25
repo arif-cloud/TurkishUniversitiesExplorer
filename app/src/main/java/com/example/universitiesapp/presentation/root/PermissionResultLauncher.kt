bdfece39a0914650a34466a56dd4873cdd2c2624
Arif Demiryas <73065590+arif-cloud@users.noreply.github.com>
2024-04-25 15:35:33 +0300

    permissionResultLauncher function updated

--- app/src/main/java/com/example/universitiesapp/presentation/root/PermissionResultLauncher.kt
+++ app/src/main/java/com/example/universitiesapp/presentation/root/PermissionResultLauncher.kt
@@ -1,19 +1,44 @@
 package com.example.universitiesapp.presentation.root
 
-import android.widget.Toast
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
 import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
+import kotlinx.coroutines.launch
 
 @Composable
-fun permissionResultLauncher(): ManagedActivityResultLauncher<String, Boolean> {
+fun permissionResultLauncher(
+    snackBarHostState: SnackbarHostState,
+): ManagedActivityResultLauncher<String, Boolean> {
     val context = LocalContext.current
+    val scope = rememberCoroutineScope()
     return rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestPermission(),
         onResult = { isGranted ->
-            Toast.makeText(context, "Call phone permission : $isGranted", Toast.LENGTH_SHORT).show()
+            if (!isGranted) {
+                scope.launch {
+                    val result = snackBarHostState.showSnackbar(
+                        message = "You should give call permission",
+                        actionLabel = "Go to Settings",
+                        duration = SnackbarDuration.Short
+                    )
+                    if (result == SnackbarResult.ActionPerformed) {
+                        val intent = Intent(
+                            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.fromParts("package", context.packageName, null)
+                        )
+                        context.startActivity(intent)
+                    }
+                }
+            }
         }
     )
 }
